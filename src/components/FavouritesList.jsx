18930e09a89c40ca7787e4e4a57507a10ceb552b
Yevhenii <zhestav20@gmail.com>
2022-05-22 18:20:19 +0200

    create my favorites list

--- src/components/FavouritesList.jsx
+++ src/components/FavouritesList.jsx
@@ -1,23 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {fsParagraph} from "../style/fsParagraph";
-import {ListGroup} from "react-bootstrap";
+import {ListGroup, Table} from "react-bootstrap";
 import axios from "axios";
+import {useFetchData} from "../hooks/useFetchData";
+import {styleWidth} from "../style/styleWidth";
+import {LinkContainer} from "react-router-bootstrap";
+import {fsSmall} from "../style/fsSmall";
+import {format, parseISO} from "date-fns";
 
 const FavouritesList = () => {
 
-    const [favorite, setFavorite] = useState()
-    const [advert, setAdvert] = useState([])
-
-    useEffect(async () => {
-        const response = await axios.get(`/adverts`)
-        setAdvert(response.data)
-    },[])
+    const [favorite, setFavorite] = useState([])
+    const [adverts] = useFetchData(`/adverts`, [])
 
     useEffect(async () => {
         const response = await axios.get('/favourites')
-        console.log(response.data)
         setFavorite(response.data)
     },[])
+
+    /*empty state, loading state, error state, fulfilled state*/
+    /*popup 30sek "kup teraz!"*/
 
     return (
         <div>
@@ -26,7 +28,36 @@ const FavouritesList = () => {
             </p>
             <ListGroup className='mt-3'>
                 {
-
+                    adverts
+                        .filter((item) => favorite.includes(item.id))
+                        .map((advert) => (
+                            <ListGroup.Item action className='mb-2' key={advert.id}>
+                                <Table size=''>
+                                    <tbody>
+                                    <tr>
+                                        <td rowSpan={2} className="border-0" style={styleWidth}>
+                                            <img src={advert.image} style={styleWidth}/>
+                                        </td>
+                                        <LinkContainer to={`/advert/${advert.id}`}>
+                                            <td valign='top' className="border-0">
+                                                {advert.title}
+                                            </td>
+                                        </LinkContainer>
+                                        <td className="border-0 text-end">
+                                            {advert.price} zł.
+                                            <p className='text-secondary'
+                                               style={fsSmall}>{advert.canNegotiate ? "To negotiate" : ""}</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td valign='bottom' className="border-0">
+                                            {format(parseISO(advert.createdOn), 'yyyy-MM-dd')}
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </Table>
+                            </ListGroup.Item>
+                        ))
                 }
             </ListGroup>
         </div>
